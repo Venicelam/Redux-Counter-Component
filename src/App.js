9544fd643b2f793175d2d207c7f23656525abbf5
lamve2 <venice.lam@oocl.com>
2018-12-04 15:39:14 +0800

    add counter according to input number

--- src/App.js
+++ src/App.js
@@ -5,12 +5,30 @@ import Counter from './Components/Counter.js'
 import CounterGroup from './Components/CounterGroup.js'
 
 class App extends Component {
-   
+    constructor(props) {
+        super(props);
+        this.state = {
+          count: '',
+          items:[]
+        };       
+      }
+    
+    onChange = (event) => {
+        this.setState({ count: event.target.value });
+      }
+    
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+          count: '',
+          items: [...this.state.counter]
+        });
+      } 
   render() {
     return (
     <div>
-   
-      <CounterGroup size={3}/>
+  
+    <CounterGroup size={3}/>
     </div>
     );
   }
--- src/Components/CounterGroup.js
+++ src/Components/CounterGroup.js
@@ -2,20 +2,33 @@ import React, { Component } from 'react'
 import Counter from './Counter.js'
 
 class CounterGroup extends Component {
-  constructor(prop) {
-    super(prop)
+  constructor(props) {
+    super(props)
     this.state = {
-        counter: new Array(prop.size).fill(0),
+      counterSum: 0,
+      count: 0,
+        counterArr: new Array(parseInt(this.props.size)).fill(0),
         sum:0
     }
   }
+  regenerateCounters =()=> {
+    this.setState({
+      counterArr: new Array(parseInt(this.refs.countInput.value)).fill(0),
+      counterSum: 0
+    });
+  }
+
   updateSum=(delta) => {
     this.setState({sum: this.state.sum+delta})
   }
   render() {
     return (
       <div>
-        {this.state.counter.map(() => <Counter onUpdate={this.updateSum}/>)}
+        <input type="text" ref="countInput" />
+        <button onClick ={this.regenerateCounters}>
+        Create Counter
+        </button>
+        {this.state.counterArr.map(() => <Counter onUpdate={this.updateSum}/>)}
         <span>sum: {this.state.sum}</span>
       </div>
     )
